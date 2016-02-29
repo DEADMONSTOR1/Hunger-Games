d9b9f56121850968c9f445d7dd98d1dd946706fe
DEADMONSTOR <josh14m@aol.com>
2016-02-29 14:04:41 +0000

    Update sv_deathmatch.lua

--- gamemode/deathmatch/sv_deathmatch.lua
+++ gamemode/deathmatch/sv_deathmatch.lua
@@ -10,23 +10,23 @@ function ResetAllTimers()
 end
 
 function DeathMatchCheck()
-  if roundTimeLeft == roundTime / 4 then
-    if roundtext == "Round is now Active" then
-    	 timer.Create( "DeathMatchTimer", 1, 4, function() DeathMatchTimer = DeathMatchTimer - 1 end )
-    	 round.BroadCast("DeathMatch Starts In" ..DeathMatchTimer)
-      for k,v in pairs(player.GetAll()) do
-        if ( v:Alive() ) then 
-          for i,p in pairs(DeathMatchPos)
-            v:SetPos(DeathMatchPos[i])
-            v:Freeze( true ) 
-            roundtext = "Round Active - DeathMatch"
-            timer.Simple(5, function()
+	if roundTimeLeft == roundTime / 4 then
+		if roundtext == "Round is now Active" then
+			timer.Create( "DeathMatchTimer", 1, 4, function() DeathMatchTimer = DeathMatchTimer - 1 end )
+			round.BroadCast("DeathMatch Starts In" ..DeathMatchTimer)
+			for k,v in pairs(player.GetAll()) do
+				if ( v:Alive() ) then 
+					for i,p in pairs(DeathMatchPos)
+						v:SetPos(DeathMatchPos[i])
+						v:Freeze( true ) 
+						roundtext = "Round Active - DeathMatch"
+							timer.Simple(5, function()
 							v:Freeze( false )
-						end)
+							end)
 					end
-        end      
-      end
-    end
-  end
+				end      
+			end
+		end
+	end
 end
 hook.Add( "Think", "DeathMatchTimeHook", DeathMatchCheck )
