0b292af507a49028228b3a310f25ddb9c6b6eb82
DEADMONSTOR <josh14m@aol.com>
2016-02-29 13:34:44 +0000

    Update init.lua

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -6,6 +6,7 @@ include( "shared.lua" )
 include( "round/sv_round.lua" )
 include( "level/expsys.lua" )
 include( "spectating/sv_spectating.lua" )
+include( "deathmatch/sv_deathmatch.lua" )
 
 allweapons = {
 	"weapon_rpg",
